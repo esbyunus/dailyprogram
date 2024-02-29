3f20676de0538e716aec9b8bbde3258239a1547e
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-02-29 22:46:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace WeeklyUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Day: ");
+            string day = Console.ReadLine().ToLower();
+
+            switch(day)
+            {
+                case "monday":
+                    Console.WriteLine("plan 1");
+                    break;
+                case "tuesday":
+                    Console.WriteLine("plan 2");
+                    break;
+                case "wednesday":
+                    Console.WriteLine("plan 3");
+                    break;
+                case "thursday":
+                    Console.WriteLine("plan 4");
+                    break;
+                case "friday":
+                    Console.WriteLine("plan 5");
+                    break;
+                case "saturday":
+                    Console.WriteLine("weekend plan 1");
+                    break;
+                case "sunday":
+                    Console.WriteLine("weekend plan 2");
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+
+            }
+
+        }
+    }
+}
